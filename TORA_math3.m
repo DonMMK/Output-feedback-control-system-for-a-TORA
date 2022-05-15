70d5f7552ca66a0410085b2cd080c649d8a97751
don <misurakalu@gmail.com>
2022-05-16 00:02:43 +1000

    Calculated the eigenvalues

--- TORA_math3.m
+++ TORA_math3.m
@@ -11,3 +11,20 @@ beta_a = (-k * (J + m *(L*L)) ) / (  (J + m * (L^2) ) *(m+M) - (m^2)*(L^2)  )
 alpha_b = (-m * L * k )  / ( (J + m * (L^2) ) *(m+M) - (m^2)*(L^2) )
 
 beta_b =  (-k * (J + m *(L*L)) ) / (  (J + m * (L^2) ) *(m+M) - (m^2)*(L^2)  )
+
+
+Aa_matrix = [0 , 1 , 0 ,0 ; 
+   0 , 0 , alpha_a , 0;
+   0 ,0 ,0 ,1;
+   0 , 0, beta_a , 0;]
+
+eig(Aa_matrix)
+
+
+Ab_matrix = [0 , 1 , 0, 0;
+    0 ,0 , alpha_b , 0;
+    0 ,0 ,0 , 1;
+    0 , 0, beta_b , 0;]
+
+eig(Ab_matrix)
+
